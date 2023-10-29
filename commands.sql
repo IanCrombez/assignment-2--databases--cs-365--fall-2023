74a3674bd79a2995b766e4536ddd0c7492eaf602
IanCrombez <icrombez@crombezclan.com>
2023-10-29 12:10:05 -0400

    made command 4/ fixed a few things

--- commands.sql
+++ commands.sql
@@ -1,12 +1,12 @@
 --new entry
 INSERT INTO web_site (WEB_name, WEB_url, name_first, name_last, username, email, password, comment) VALUES
-    ('INSERTEDSITE', 'http://www.insert.com', 'Code', 'Warrior', 'code-warrior', 'spooky@hartofrd.edu', AES_ENCRYPT('cs-365', @key_str, @init_vector), 'this was the inserted webiset from the command');
+    ('INSERTEDSITE', 'https://www.insert.com', 'Code', 'Warrior', 'code-warrior', 'spooky@hartofrd.edu', AES_ENCRYPT('cs-365', @key_str, @init_vector), 'this was the inserted webiset from the command');
 --get password
 SELECT CAST(AES_DECRYPT(password, @key_str, @init_vector)AS CHAR) AS real_password FROM web_site WHERE WEB_url = 'http://www.testsite.com';
 --get all data from https sites
-SELECT *, CAST(AES_DECRYPT(password, @key_str, @init_vector)AS CHAR) AS real_password FROM web_site WHERE WEB_url = 'https://%';
---change a url of an entry
-
+SELECT *, CAST(AES_DECRYPT(password, @key_str, @init_vector)AS CHAR) AS real_password FROM web_site WHERE WEB_url LIKE 'https://%';
+--change a url of an entry based on password
+UPDATE web_site SET WEB_url = 'http://www.updatingSiteBasedonPassword.com' WHERE password = AES_ENCRYPT('1235', @key_str, @init_vector);
 --change any password
 
 --remove a url
